8e4e23828b053390fda4dbb324fa15a0085f3e72
Javier Palacios Botejara <javierpalaciosbotejara@gmail.com>
2021-11-20 14:00:18 +0100

    Add manual router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
-
+import Main from './component/Main';
+import ShowLocals from './component/ShowLocals';
+import UniqueLocal from './component/UniqueLocal';
+ 
 function App() {
+  const main = false;
+  const showLoclas = false;
+  const uniqueLocals = false;
   return (
     <div className="App">
-    
+      {main && <Main />}
+      {showLoclas && <ShowLocals/>}
+      {uniqueLocals && <UniqueLocal/>}
     </div>
   );
 }
-
+ 
 export default App;
