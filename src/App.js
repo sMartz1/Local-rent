e27dca64ab7fb46e937d6aa63979c24d97ebe150
Javier Palacios Botejara <javierpalaciosbotejara@gmail.com>
2021-11-23 19:25:01 +0100

    fix folders names

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import './App.css';
-import Main from './component/Main';
-import ShowLocals from './component/ShowLocals';
-import UniqueLocal from './component/UniqueLocal';
+import Main from './components/Main';
+import ShowLocals from './components/ShowLocals';
+import UniqueLocal from './components/UniqueLocal';
 import Header from './components/Header';
 
 function App() {
     const main = false;
     const showLocals = false;
     const uniqueLocals = false;
-    return ( <
-        div className = "App" >
-        <
-        Header / > { main && < Main / > } { showLocals && < ShowLocals / > } { uniqueLocals && < UniqueLocal / > } <
-        /div>
+    return ( 
+      <div className = "App" >
+        <Header /> { main && < Main /> } { showLocals && < ShowLocals /> } { uniqueLocals && < UniqueLocal /> } 
+       </div>
     );
 }
 
