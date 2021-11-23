18f2a0a0f12e6b6e163ba5220d3fb172de08e65f
Sebastian M <ra3gan1991@gmail.com>
2021-11-23 11:35:13 +0100

    We create RESULTS component

---
+++ src/components/ShowLocals/Results/Results.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import './component.css'
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { }
+    }
+    render() { 
+        return (<div className="results-container">
+            
+        </div>  );
+    }
+}
+ 
+export default Header;
