99cfb8c9ec29ac169a40eb7c4b6959cf2ada4b43
Sebastian M <ra3gan1991@gmail.com>
2021-11-23 20:57:45 +0100

    Standarize UNIQUE_LOCAL component

---
+++ src/components/UniqueLocal/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './component.css'
+class UniqueLocal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (<div className="unique-local" id="map">
+        </div>  );
+    }
+}
+ 
+export default UniqueLocal;
