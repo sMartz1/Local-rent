ed7953d34beb2e0c9e3201b2c4ed16b27f43f84e
Sebastian M <ra3gan1991@gmail.com>
2021-11-23 11:14:55 +0100

    We create SHOW_LOCAL component

---
+++ src/components/ShowLocals/ShowLocals.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './component.css'
+
+
+class ShowLocals extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <div className="show-locals-container">
+            
+        </div> );
+    }
+}
+ 
+export default ShowLocals;
