df1dce354fefbebf6e20f713da43a59957e41552
Sebastian M <ra3gan1991@gmail.com>
2021-11-23 18:23:23 +0100

    We create MAIN component

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Header from '../Header'
+import './component.css'
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { }
+    }
+    render() { 
+        return (<div className="main-container">
+        </div>  );
+    }
+}
+ 
+export default Main;
