5bb94435dff23a83d320b1918556a02b3768bfee
Javier Palacios Botejara <javierpalaciosbotejara@gmail.com>
2021-11-25 19:53:24 +0100

    add mock data

--- src/data/localInfo.js
+++ src/data/localInfo.js
@@ -3,4 +3,5 @@ export const locals = [
     {id:'2', img:'img', name:'local2', price:'20€/h', type:'type', size:'size', description:'description'},
     {id:'3', img:'img', name:'local3', price:'12€/h', type:'type', size:'size', description:'description'},
     {id:'4', img:'img', name:'local4', price:'5€/h', type:'type', size:'size', description:'description'},
+    {id:'5', img:'img', name:'local4', price:'5€/h', type:'type', size:'size', description:'description'},
 ]
