a983079cbe81333bb47e9c45b579bffa97009b9c
Javier Palacios Botejara <javierpalaciosbotejara@gmail.com>
2021-11-25 19:51:58 +0100

    fix merging problems

--- src/data/localInfo.js
+++ src/data/localInfo.js
@@ -1,6 +1,6 @@
 export const locals = [
-    {id:'1', img:'img', name:'local1', price:'10€/h', type:'type', size:'size'},
-    {id:'2', img:'img', name:'local2', price:'20€/h', type:'type', size:'size'},
-    {id:'3', img:'img', name:'local3', price:'12€/h', type:'type', size:'size'},
-    {id:'4', img:'img', name:'local4', price:'5€/h', type:'type', size:'size'},
+    {id:'1', img:'img', name:'local1', price:'10€/h', type:'type', size:'size', description:'description'},
+    {id:'2', img:'img', name:'local2', price:'20€/h', type:'type', size:'size', description:'description'},
+    {id:'3', img:'img', name:'local3', price:'12€/h', type:'type', size:'size', description:'description'},
+    {id:'4', img:'img', name:'local4', price:'5€/h', type:'type', size:'size', description:'description'},
 ]
