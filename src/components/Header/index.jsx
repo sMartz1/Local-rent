94344579c61fecb787e815cdfd77a89c738ab64b
Javier Palacios Botejara <javierpalaciosbotejara@gmail.com>
2021-12-11 11:53:10 +0100

    add search on scroll

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,22 +8,22 @@ class Header extends Component {
         super(props);
         this.toggleSearch = this.toggleSearch.bind(this);
         this.state = { logoImage : "path", title:"Local RENT",
-                       showButton: this.props.showSeacrh, showModal:false }
+                       showButton: false, showModal:false }
     }
 
     toggleSearch(){
-        this.setState(state=>({...state, showModal: !this.state.showModal}))
+        this.setState(state=>({...state, showModal: !this.state.showModal}));
     }
 
     
 
     render() { 
         window.onscroll = ()=>{
-            console.log(document.body.scrollHeight);
-            if(document.body.scrollHeight < 200){
+            if(window.pageYOffset > 235){
                 this.setState(state=>({...state, showButton: true}))
             } else {
                 this.setState(state=>({...state, showButton: false}))
+                this.setState(state=>({...state, showModal: false}))
             }
         };
         return (<div className="header">
@@ -31,12 +31,15 @@ class Header extends Component {
             <img src={this.state.logoImage} alt="" />
             {
                 this.state.showButton && 
-                <button onClick={this.toggleSearch}>
+                <button className="show-search-modal" onClick={this.toggleSearch}>
                     Buscar <img src={search} alt="open search modal" />
                 </button>
             }
             
-            {this.state.showModal && <div className="search-modal"><SearchSection/></div>}
+            {(this.state.showModal && this.props.showSeacrh) && 
+            <div className="search-modal">
+                <SearchSection/>
+            </div>}
         </div>  );
     }
 }
--- src/data/localInfo.js
+++ src/data/localInfo.js
@@ -1,7 +1,8 @@
 export const locals = [
-    {id:'1', img:'img', name:'local1', price:'10€/h', type:'type', size:'size', description:'description'},
-    {id:'2', img:'img', name:'local2', price:'20€/h', type:'type', size:'size', description:'description'},
-    {id:'3', img:'img', name:'local3', price:'12€/h', type:'type', size:'size', description:'description'},
-    {id:'4', img:'img', name:'local4', price:'5€/h', type:'type', size:'size', description:'description'},
-    {id:'5', img:'img', name:'local4', price:'5€/h', type:'type', size:'size', description:'description'},
+    {id:'1', img:'img', title:'local1', price:'10€/h', type:'tienda', size:323, description:'description', featured: true, sale: 0},
+    {id:'2', img:'img', title:'local2', price:'20€/h', type:'interior', size:5000, description:'description', featured: true, sale: 0},
+    {id:'3', img:'img', title:'local3', price:'12€/h', type:'exterior', size:233, description:'description', featured: true, sale: 0},
+    {id:'4', img:'img', title:'local4', price:'15€/h', type:'industrial', size:10000, description:'description', featured: false, sale: 20},
+    {id:'5', img:'img', title:'local5', price:'5€/h', type:'exterior', size:50, description:'description', featured: false, sale: 30},
+    {id:'5', img:'img', title:'local6', price:'5€/h', type:'tienda', size:50, description:'description', featured: false, sale: 10},
 ]
